66ef8bc7b393fda9b8c6ddebb5df9c02fdad0729
CSaymon <81566299+CSaymon@users.noreply.github.com>
2021-04-05 09:48:47 -0300

    RollDice

--- dado.js
+++ dado.js
@@ -5,23 +5,23 @@ var sorteio = Math.floor(Math.random () * 6 + 1);
 
 switch(sorteio){
     case 1:
-            document.getElementById("face").src = "um.png";
+            document.getElementById("face").src = "./img/um.png";
             break;
     case 2:
-            document.getElementById("face").src = "dois.png";
+            document.getElementById("face").src = "./img/dois.png";
             break;        
     case 3:
-                document.getElementById("face").src = "tres.png";
+                document.getElementById("face").src = "./img/tres.png";
                 break;
     case 4:
-                    document.getElementById("face").src = "quatro.png";
+                    document.getElementById("face").src = "./img/quatro.png";
                     break;  
                     
     case 5:
-                        document.getElementById("face").src = "cinco.png";
+                        document.getElementById("face").src = "./img/cinco.png";
                         break;                
     case 6:
-                            document.getElementById("face").src = "seis.png";
+                            document.getElementById("face").src = "./img/seis.png";
                             break;
     default:
         break;
